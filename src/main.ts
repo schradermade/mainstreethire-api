e565a82b0bc94f2b7f49e33c74ada860d57bd641
Nathan Schrader <natecschrader@gmail.com>
2024-11-20 13:19:58 -0800

    small change to test deployment

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
   await app.listen(port);
 
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`App is running on: ${await app.getUrl()}`);
 }
 bootstrap();
