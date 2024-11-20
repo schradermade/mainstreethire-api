8a677453c05279d34b85cb5b307ee64370dfdc6c
Nathan Schrader <natecschrader@gmail.com>
2024-11-20 14:03:46 -0800

    update main.ts to work with vercel deploy

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,32 @@
+import { join } from 'path';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(server),
+  );
+
+  // Serve static files from the "public" directory
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  // Enable CORS
   app.enableCors();
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  // Initialize the app without binding to a specific port
+  await app.init();
 
-  console.log(`App is running on: ${await app.getUrl()}`);
+  console.log(`Application initialized and ready to handle requests`);
 }
+
+// Start the app
 bootstrap();
+
+// Export the server for Vercel compatibility
+export default server;
