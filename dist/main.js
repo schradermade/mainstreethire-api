dd5f4d821dd90edf8e85134cc79c3dc5d423a0a9
Nathan Schrader <natecschrader@gmail.com>
2024-11-21 10:01:02 -0800

    npm run build - add dist directory

---
+++ dist/main.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = require("path");
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+const platform_express_1 = require("@nestjs/platform-express");
+const express = require("express");
+const server = express();
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter(server));
+    app.useStaticAssets((0, path_1.join)(__dirname, '..', 'public'));
+    app.enableCors();
+    console.log('__dirname:', __dirname);
+    await app.init();
+    console.log(`Application initialized and ready to handle requests`);
+}
+bootstrap();
+exports.default = server;
+//# sourceMappingURL=main.js.map
