ced690c13999f4455de430efdea1d3488cb7e2ab
Nathan Schrader <natecschrader@gmail.com>
2024-11-21 12:32:50 -0800

    move api dir into src

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
 import { createServer } from '@vercel/node';
-import { app } from '../dist/main'; // Adjust the path to your dist/main
+import { app } from '../../dist/main'; // Adjust the path to your dist/main
 
 export default createServer(app);
